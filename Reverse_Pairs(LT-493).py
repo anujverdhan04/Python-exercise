db638b425dc90374492829762fcb6432028b4e8f
Anuj Verdhan <114421335+anujverdhan04@users.noreply.github.com>
2024-05-27 01:31:27 +0530

    Add files via upload

---
+++ Reverse_Pairs(LT-493).py
@@ -0,0 +1,12 @@
+class Solution:
+
+    def reversePairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        count =0
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if(nums[i] >2*nums[j]):
+                    count +=1
+        return count            
+
+        
