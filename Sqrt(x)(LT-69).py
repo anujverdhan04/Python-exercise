e20933dcb4d3ee97c18e2dfba807ab1be46ff89c
Anuj Verdhan <114421335+anujverdhan04@users.noreply.github.com>
2024-08-19 20:14:57 +0530

    Add files via upload

---
+++ Sqrt(x)(LT-69).py
@@ -0,0 +1,10 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        result = int(x ** .5)
+        return result
+
+'''       
+        result = int(math.sqrt(x))
+        return result
+'''
+        
